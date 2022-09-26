70660dd2bc0665b2d8842de3c69e8d24ea358fa3
shizhiheng <2225018807@qq.com>
2022-09-26 11:27:06 +0800

    修改端口

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ docker stop vuecontainer;
 docker rm vuecontainer;
 docker build -t vueimage:$image_version .;
 docker images;
-docker run -p 80:8081 -d --name vuecontainer vueimage:$image_version;
+docker run -p 8081:8081 -d --name vuecontainer vueimage:$image_version;
 docker logs vuecontainer;
 # 1、删除所有悬空镜像，不删除未使用镜像：docker rmi $(docker images -f "dangling=true" -q)
 # 2.删除所有未使用镜像和悬空镜像：docker rmi $(docker images -q)
