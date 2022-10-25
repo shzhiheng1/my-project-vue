63d412f83ffb5cfc3bacabe878ef6b80bd50eba1
shizhiheng <2225018807@qq.com>
2022-10-25 09:34:00 +0800

    修改dockerfile

--- setup.sh
+++ setup.sh
@@ -9,4 +9,4 @@ docker run -p 8081:8081 -d --name vuecontainer vueimage:$image_version;
 docker logs vuecontainer;
 # 1、删除所有悬空镜像，不删除未使用镜像：docker rmi $(docker images -f "dangling=true" -q)
 # 2.删除所有未使用镜像和悬空镜像：docker rmi $(docker images -q)
-# docker rmi $(docker images -f "dangling=true" -q);
+docker rmi $(docker images -f "dangling=true" -q);
